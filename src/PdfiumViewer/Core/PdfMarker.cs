7509e450307bd02bde317e2504f822ffc57f3214
Behzad <behzad.khosravifar@gmail.com>
2020-07-17 17:54:31 +0430

    Added Markers for search and highlights

---
+++ src/PdfiumViewer/Core/PdfMarker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using PdfiumViewer.Drawing;
+using Color = System.Windows.Media.Color;
+using Pen = System.Windows.Media.Pen;
+
+namespace PdfiumViewer.Core
+{
+    public class PdfMarker : IPdfMarker
+    {
+        public int Page { get; }
+        public RectangleF Bounds { get; }
+        public Color Color { get; }
+        public Color BorderColor { get; }
+        public float BorderWidth { get; }
+
+        public PdfMarker(int page, RectangleF bounds, Color color)
+            : this(page, bounds, color, Colors.Transparent, 0)
+        {
+        }
+
+        public PdfMarker(int page, RectangleF bounds, Color color, Color borderColor, float borderWidth)
+        {
+            Page = page;
+            Bounds = bounds;
+            Color = color;
+            BorderColor = borderColor;
+            BorderWidth = borderWidth;
+        }
+
+        public void Draw(PdfRenderer renderer, DrawingContext graphics)
+        {
+            if (renderer == null)
+                throw new ArgumentNullException(nameof(renderer));
+            if (graphics == null)
+                throw new ArgumentNullException(nameof(graphics));
+
+            Rect bounds = renderer.BoundsFromPdf(new PdfRectangle(Page, Bounds));
+            var brush = new SolidColorBrush(Color) { Opacity = .8 };
+            var pen = new Pen(new SolidColorBrush(BorderColor) { Opacity = .8 }, BorderWidth);
+            graphics.DrawRectangle(brush, null, bounds);
+
+            if (BorderWidth > 0)
+            {
+                graphics.DrawRectangle(null, pen, bounds);
+            }
+        }
+    }
+}
---
+++ src/PdfiumViewer/Core/PdfMarkerCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace PdfiumViewer.Core
+{
+    public class PdfMarkerCollection : Collection<IPdfMarker>
+    {
+        public event EventHandler CollectionChanged;
+
+        protected override void ClearItems()
+        {
+            base.ClearItems();
+
+            OnCollectionChanged(EventArgs.Empty);
+        }
+
+        protected override void InsertItem(int index, IPdfMarker item)
+        {
+            base.InsertItem(index, item);
+
+            OnCollectionChanged(EventArgs.Empty);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            base.RemoveItem(index);
+
+            OnCollectionChanged(EventArgs.Empty);
+        }
+
+        protected override void SetItem(int index, IPdfMarker item)
+        {
+            base.SetItem(index, item);
+
+            OnCollectionChanged(EventArgs.Empty);
+        }
+
+        protected virtual void OnCollectionChanged(EventArgs e)
+        {
+            CollectionChanged?.Invoke(this, e);
+        }
+    }
+}
