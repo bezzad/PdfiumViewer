e1e09eb9a2e2fd09dc6ddc900bce53be527e1371
Behzad <behzad.khosravifar@gmail.com>
2020-10-26 23:38:56 +0330

    Added Bookmarks TOC to demo window

---
+++ src/PdfiumViewer.Demo/Converters/BehaviorForStyle.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace PdfiumViewer.Demo.Converters
+{
+    /// <summary>
+    /// Wrapper to allow a behavior to be included in a style definition
+    /// </summary>
+    /// <typeparam name="TTarget"></typeparam>
+    /// <typeparam name="TBehavior"></typeparam>
+    public class BehaviorForStyle<TTarget, TBehavior> : Behavior<TTarget>
+        where TTarget : DependencyObject
+        where TBehavior : BehaviorForStyle<TTarget, TBehavior>, new()
+    {
+        public static readonly DependencyProperty IsEnabledForStyleProperty =
+            DependencyProperty.RegisterAttached("IsEnabledForStyle",
+                typeof(bool),
+                typeof(BehaviorForStyle<TTarget, TBehavior>),
+                new FrameworkPropertyMetadata(false, OnIsEnabledForStyleChanged));
+
+        public bool IsEnabledForStyle
+        {
+            get => (bool)GetValue(IsEnabledForStyleProperty);
+            set => SetValue(IsEnabledForStyleProperty, value);
+        }
+
+        private static void OnIsEnabledForStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(e.NewValue is bool))
+                return;
+
+            var newValue = (bool)e.NewValue;
+
+            var behaviors = Interaction.GetBehaviors(d);
+            var existingBehavior = behaviors.FirstOrDefault(b => b.GetType() == typeof(TBehavior)) as TBehavior;
+
+            if (!newValue && existingBehavior != null)
+                behaviors.Remove(existingBehavior);
+            else if (newValue && existingBehavior == null)
+                behaviors.Add(new TBehavior());
+        }
+    }
+}
