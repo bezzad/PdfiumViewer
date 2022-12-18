2d64d8425ac78bccfa0f587abd147003ada85531
saeedeh <saeedeh.amini3000@gmail.com>
2022-12-18 16:12:02 +0330

    Fixed view binders

---
+++ src/PdfiumViewer.Demo/Converters/StringToIntConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PdfiumViewer.Demo.Converters
+{
+    public class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (int.TryParse(value.ToString(), out int number))
+            {
+                return number;
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() ?? "0";
+        }
+    }
+}
